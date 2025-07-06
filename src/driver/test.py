fcb9f1b1c4cca40029f935d7cf683ab94f265605
zhaor <zhaorc@gmail.com>
2025-07-06 10:58:21 +0800

    driver

---
+++ src/driver/test.py
@@ -0,0 +1,12 @@
+from RPi import GPIO
+import stepper
+
+def test_stepper():
+    dir_pin = 2
+    step_pin = 3
+    speed = 60
+    steps = 200 * 32
+    m1 = stepper.Stepper(dir_pin, step_pin, speed, steps)
+    m1.run(speed, steps)
+
+    GPIO.cleanup()
--- src/main.py
+++ src/main.py
@@ -1,2 +1,4 @@
-from driver import  stepper
+from driver import  stepper, test
 
+if __name__ == "__main__":
+    test.test_stepper()
