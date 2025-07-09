37fd648afff174cb9d5fa3c132fef872e4a9a90b
zhaor <zhaorc@gmail.com>
2025-07-09 23:06:27 +0800

    driver

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ def test_stepper():
     dir_pin = 2
     step_pin = 3
     relay_pin = 19
-    speed = 60
+    speed = 30
     steps = 200 * 32
     m1 = stepper.Stepper(dir_pin, step_pin, speed, steps, relay_pin)
     m1.run(speed, steps)
