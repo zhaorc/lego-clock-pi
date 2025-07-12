8650dc97501aa5c915863a382dbf66090f2b1ce6
zhaor <zhaorc@gmail.com>
2025-07-12 17:10:29 +0800

    show time

--- src/main.py
+++ src/main.py
@@ -43,8 +43,9 @@ def calculate_steps(now_time, saved_time):
 
 def show_time():
     for motor_num in (3, 2, 1, 0):
+        logger.info("motor_num={}".format(motor_num))
         saved_time_str = read_time_str()
-        now_time = time.strftime("%H")[motor_num]
+        now_time = time.strftime("%H%M")[motor_num]
         saved_time = saved_time_str[motor_num]
         run_steps = calculate_steps(now_time, saved_time)
         if run_steps is not None:
