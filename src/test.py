5247f63f2223abcb96de6d7934b05e9492a2bfde
zhaor <zhaorc@gmail.com>
2025-07-25 19:31:43 +0800

    show time

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ def test_stepper():
     dir_pin = 2
     step_pin = 3
     relay_pin = 16
-    speed = 10
+    speed = 20
     steps = 200 * 32
     run_steps = 16 * 200 * 32
     m1 = stepper.Stepper(dir_pin, step_pin, speed, steps, relay_pin)
