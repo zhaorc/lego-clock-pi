07adaf62ca90aaa39f5d109b6fe3c9d2d8ac491f
zhaor <zhaorc@gmail.com>
2025-07-27 13:29:48 +0800

    show time

--- src/test.py
+++ src/test.py
@@ -7,9 +7,8 @@ def test_stepper():
     relay_pin = 20
     speed = 20
     steps = 200 * 32
-    run_steps = 16 * 200 * 32
+    run_steps = 1 * 200 * 32
     m1 = stepper.Stepper(dir_pin, step_pin, speed, steps, relay_pin)
-    GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)
     m1.run(run_steps)
 
     GPIO.cleanup()
