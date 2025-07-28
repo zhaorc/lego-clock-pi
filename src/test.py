8cd1fbd1955448d1e1bda2f85575c908deaeac3d
zhaor <zhaorc@gmail.com>
2025-07-28 21:10:19 +0800

    show time

--- src/test.py
+++ src/test.py
@@ -1,14 +1,19 @@
 from RPi import GPIO
 from driver import stepper
+import time
 
 def test_stepper():
+    rate = 14.6
     dir_pin = 0
     step_pin = 1
     speed = 20
     steps = 200 * 32
-    run_steps = 1 * 200 * 32
+    run_steps =int(rate * 200 * 32 / 10)
     m1 = stepper.Stepper(dir_pin, step_pin, speed, steps)
-    m1.run(run_steps)
+    for i in range(10):
+        for j in range(10):
+            m1.run(run_steps)
+            time.sleep(6)
 
     GPIO.cleanup()
 
