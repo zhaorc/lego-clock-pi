2e916db89e9d91547268c338d505a4c59f797146
zhaor <zhaorc@gmail.com>
2025-08-03 14:11:22 +0800

    show time

--- src/test2.py
+++ src/test2.py
@@ -17,7 +17,7 @@ def signal_handler(sig, frame):
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(switch_pin, GPIO.IN, pull_up_down= GPIO.PUD_UP)
-    GPIO.add_event_detect(switch_pin, GPIO.RISING, callback=count_steps, bouncetime=100)
+    GPIO.add_event_detect(switch_pin, GPIO.FALLING, callback=count_steps, bouncetime=100)
     signal.signal(signal.SIGINT, signal_handler)
     while True:
         print(steps)
