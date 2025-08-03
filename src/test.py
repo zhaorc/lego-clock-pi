6649a969548d7c04e0e420b220b09de8932544cf
zhaor <zhaorc@gmail.com>
2025-08-03 17:25:38 +0800

    show time

--- src/test.py
+++ src/test.py
@@ -3,9 +3,9 @@ from driver import stepper
 import time
 
 def test_stepper():
-    dir_pin = 6
-    step_pin = 12
-    switch_pin = 3
+    dir_pin = 19
+    step_pin = 16
+    switch_pin = 4
     direction = 1
     speed = 20
     steps = 200 * 32
