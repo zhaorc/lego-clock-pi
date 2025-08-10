b4fef51fefd87a51cb9d56d90568619217ce6618
zhaor <zhaorc@gmail.com>
2025-08-10 14:42:52 +0800

    show time

--- src/test.py
+++ src/test.py
@@ -3,12 +3,12 @@ from driver import stepper
 import time
 
 def test_stepper():
-    dir_pin = 0
-    step_pin = 1
-    switch_pin = 2
+    dir_pin = 26
+    step_pin = 20
+    switch_pin = 17
     direction = 1
     speed = 20
-    steps = 200 * 32
+    steps = 200 * 128
     run_distance =1 * direction
     m1 = stepper.Stepper(dir_pin, step_pin, switch_pin, speed, steps)
     for i in range(10):
